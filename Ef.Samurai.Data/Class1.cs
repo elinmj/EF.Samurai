f9d4ea36ca1060e07aef1168c7458022e12bfacf
Elin Johansson <elinmariejohansson@gmail.com>
2019-01-17 13:31:40 +0100

    Add project files.

---
+++ Ef.Samurai.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ef.Samurai.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Ef.Samurai.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ef.Samurai.Domain
+{
+    public class Class1
+    {
+    }
+}
